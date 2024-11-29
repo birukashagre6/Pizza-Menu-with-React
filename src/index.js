2d3ac5717bb319fb5faf0cfa6cb731700679f850
birukashagre6 <mandave693@gmail.com>
2024-11-29 22:59:17 +0300

    adding css to style and props to pass data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import "./index.css";
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -11,26 +11,54 @@ function App() {
   );
 }
 function Header() {
-  return <h1>Wellcom to Fast Pizza</h1>;
+  const style = {};
+  return (
+    <header className="header">
+      <h1 style={style}>Fast React Pizza</h1>
+    </header>
+  );
 }
 function Menu() {
   return (
-    <div>
+    <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />;
-      <Pizza />;
-      <Pizza />;
+      <Pizza
+        name="pizza Spinaci"
+        ingredients="Tomato,mozarella, spinach and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+      <Pizza
+        name="pizza Funghi"
+        ingredients="Tomato,mozarella, mushrooms"
+        photoName="pizzas/funghi.jpg"
+        price={12}
+      />
+    </main>
+  );
+}
+
+function Pizza(props) {
+  console.log(props);
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 3}</span>
+      </div>
     </div>
   );
 }
 function Footer() {
   return (
-    <footer>{new Date().toLocaleDateString()}. We're currently open</footer>
+    <footer className="footer">
+      {new Date().toLocaleDateString()}. We're currently open
+    </footer>
   );
 }
-function Pizza() {
-  return <img src="pizzas/spinaci.jpg" alt="pizza spinaci" />;
-}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
